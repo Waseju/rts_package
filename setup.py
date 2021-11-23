34411f217859d743cc8a83769e365b95f25cb517
Waseju <jwgithub@mailbox.org>
2021-11-23 20:44:39 +0100

    Bump version from 1.1.11 to 1.1.12

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     setup_requires=setup_requirements,
     tests_require=test_requirements,
     url='https://github.com/waseju/rts_package',
-    version='1.1.11',
+    version='1.1.12',
     zip_safe=False,
 )
